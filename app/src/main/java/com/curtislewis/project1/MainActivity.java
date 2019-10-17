ed811878dde075113e990a6f683e2496d06ccfe7
LAPRAS\Curtis <insanendmembrane101@gmail.com>
2019-10-17 09:27:54 -0400

    second project commit

--- app/src/main/java/com/curtislewis/project1/MainActivity.java
+++ app/src/main/java/com/curtislewis/project1/MainActivity.java
@@ -2,24 +2,43 @@ package com.curtislewis.project1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toolbar;
 import android.widget.CalendarView;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import org.mariuszgromada.math.mxparser.Expression;
 
 import java.lang.reflect.Array;
 
 
 public class MainActivity extends AppCompatActivity {
+    //number for identification of this activity for image
+    final int CallerCode = 110;
+
+    //image object
+    ImageView pictureView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
+        //CODE FOR CALENDAR VIEW
         CalendarView cv = (CalendarView) findViewById(R.id.calendarView);
         final TextView textView = (TextView) findViewById(R.id.textDate);
 
@@ -67,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 });
             } else if (id == R.id.buttonEq) {
 
-                final Button buttonEq= findViewById(R.id.buttonEq);
+                final Button buttonEq = findViewById(R.id.buttonEq);
                 buttonEq.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -84,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                         Double result = expression.calculate();
 
                         //shows the results to the textfield
-                        text.setText(""+result);
+                        text.setText("" + result);
 
                     }
                 });
@@ -117,6 +136,68 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+
+
+
+        //links button to camera view xml
+        Button button = findViewById(R.id.SnapButton);
+
+        //links image to area of camera view xml file to be displayed
+        pictureView = findViewById(R.id.ImageView1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //line says i wan this object to use the camera provided by hardware
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                //starts the activity to take picture and return it to this app as the caller
+                startActivityForResult(intent, CallerCode);
+            }
+        });
+
+        //create connection to the user interface
+
+        final WebView webView = findViewById(R.id.webView1);
+
+        Button button1 = findViewById(R.id.GoButton);
+
+        final EditText editText = findViewById(R.id.editText1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = editText.getText().toString();
+                webView.setWebViewClient(new WebViewClient());
+                webView.loadUrl(url);
+
+
+            }
+        });
+
+
+
+    }
+
+            @Override
+            protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+                //if request code and Caller of Intent matches
+                if (requestCode == CallerCode) {
+
+                    //if result successful retrieve image
+                    if (resultCode == RESULT_OK) {
+                        Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+                        pictureView.setImageBitmap(bitmap);
+
+                    } else if (resultCode == RESULT_CANCELED) {
+                        View parentView = pictureView.getRootView();
+                        Snackbar.make(parentView, "snap cancelled.", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
     }
 
 
@@ -126,4 +207,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-}
+
